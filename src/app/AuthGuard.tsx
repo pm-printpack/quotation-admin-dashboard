e6393838a39b5cf091c53e3be874152064149050
Cory <cory@rotender.com>
2025-06-23 18:25:43 -0700

    fix: remove "/" from the end of paths

--- src/app/AuthGuard.tsx
+++ src/app/AuthGuard.tsx
@@ -29,7 +29,7 @@ export default function AuthGuard({ children }: PropsWithChildren) {
 
   useEffect(() => {
     const token: string | null = localStorage.getItem("jwtToken");
-    const isLoginPage = pathname === "/login/";
+    const isLoginPage = pathname === "/login";
     let isValid: boolean = false;
 
     if (token) {
@@ -45,10 +45,10 @@ export default function AuthGuard({ children }: PropsWithChildren) {
 
     if (!isValid && !isLoginPage) {
       // no valid token and not already on login
-      router.replace("/login/")
+      router.replace("/login")
     } else if (isValid && isLoginPage) {
       // already authenticated but on login page
-      router.replace("/dashboard/");
+      router.replace("/dashboard");
       dispatch(setAuthenticated(true));
     } else {
       // OK to render the children
--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -35,10 +35,10 @@ export default function DashboardLayout({children}: PropsWithChildren) {
 
   const menuItems: ItemType<MenuItemType>[] = useMemo(() => [
     getItem("Home", "/", <BarChartOutlined />),
-    getItem("材料管理", "/materials/", <BuildOutlined />),
-    getItem("客户管理", "/customers/", <UserOutlined />),
-    getItem("系统管理员", "/admins/", <AuditOutlined />),
-    getItem("系统设置", "/settings/", <SettingOutlined />)
+    getItem("材料管理", "/materials", <BuildOutlined />),
+    getItem("客户管理", "/customers", <UserOutlined />),
+    getItem("系统管理员", "/admins", <AuditOutlined />),
+    getItem("系统设置", "/settings", <SettingOutlined />)
   ], [getItem]);
 
   const onMenuSelect = useCallback((info: SelectInfo) => {
